a39e407d1bc6a835c50ec2d4458526f88f55bc15
Amir SHIRALI POUR <emasalari@gmail.com>
2020-11-11 15:50:32 +0100

    Semester 3

--- HMIN325M - Technologies avancées du web/creationBase.sh
+++
@@ -1,4 +0,0 @@
-mongoimport --db SUPERVENTES --collection products --file products.json --jsonArray --drop
-mongoimport --db SUPERVENTES --collection products --file hassina.json --jsonArray
-mongoimport --db SUPERVENTES --collection members --file members.json --jsonArray --drop
-
